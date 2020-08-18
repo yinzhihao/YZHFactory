efa6ba1ca2545e2b352e00a049528b9881a3ec0a
yzh115 <yzh115@zcsmart.com>
2020-08-18 18:20:14 +0800

    1.0

---
+++ YZHFactory/YZHFactory.h
@@ -0,0 +1,19 @@
+//
+//  YZHFactory.h
+//  YZHFactory
+//
+//  Created by NO NAME on 2020/8/18.
+//  Copyright © 2020 NO NAME. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YZHFactory.
+FOUNDATION_EXPORT double YZHFactoryVersionNumber;
+
+//! Project version string for YZHFactory.
+FOUNDATION_EXPORT const unsigned char YZHFactoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YZHFactory/PublicHeader.h>
+
+
